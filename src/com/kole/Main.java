1fa2b1d31e78301a975dd50cb3c48edbaed2f1dd
Slobodan <slobodan.kosanovic@gmail.com>
2018-04-06 16:04:50 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        calcFeetAndInchesToCentimeters(5);
+
+
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters (double feet, double inches) {
+        if (feet >=0 || (inches >=0 || inches <=12)){
+            double totalCentimeters = (feet * 30.48) + (inches * 2.54);
+            return totalCentimeters;
+        } else System.out.println("Feet or inches outside of boundaries");
+        return -1;
+    }
+
+    public static double calcFeetAndInchesToCentimeters (double inches){
+        if (inches >= 0){
+            double totalFeet = inches / 12;
+            double overload = calcFeetAndInchesToCentimeters(totalFeet, inches);
+            return overload;
+        } return -1;
+
     }
 }
