756bca877908b45410f2361c4607d1a72ea9bb5f
Kole <slobodan.kosanovic@gmail.com>
2018-04-07 17:28:00 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -4,26 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calcFeetAndInchesToCentimeters(5);
-
-
+        System.out.println(calcFeetAndInchesToCentimeters(221));
 
     }
 
-    public static double calcFeetAndInchesToCentimeters (double feet, double inches) {
-        if (feet >=0 || (inches >=0 || inches <=12)){
-            double totalCentimeters = (feet * 30.48) + (inches * 2.54);
-            return totalCentimeters;
-        } else System.out.println("Feet or inches outside of boundaries");
-        return -1;
+    public static double calcFeetAndInchesToCentimeters (double feet, double inches){
+        if (feet < 0 || inches <0 || inches > 12){
+            System.out.println("Invalid feet or inches");
+            return -1;
+        } else {
+            double centimeters = (feet * 12 * 2.54) + (inches * 2.54);
+            return centimeters;
+        }
     }
 
     public static double calcFeetAndInchesToCentimeters (double inches){
-        if (inches >= 0){
+        if (inches < 0){
+            System.out.println("Invalid inches");
+            return -1;
+        } else {
             double totalFeet = inches / 12;
-            double overload = calcFeetAndInchesToCentimeters(totalFeet, inches);
-            return overload;
-        } return -1;
+            double remainingInches = inches % 12;
+            System.out.println(remainingInches);
+            return calcFeetAndInchesToCentimeters(totalFeet, remainingInches);
+        }
 
     }
+
 }
